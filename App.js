734e4ff5cf2ea1b6b37b682a7737c86e32d177cf
andmiguedev <andmiguedev@outlook.com>
2022-12-19 12:43:01 -0500

    Fix linting for existing files

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { StatusBar, SafeAreaView, Text, View, StyleSheet } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+
+export default function App() {
+  return (
+    <>
+      <SafeAreaView style={styles.appContainer}>
+        <View style={styles.searchArea}>
+          <Text>Search Bar</Text>
+        </View>
+        <View style={styles.screenArea}>
+          <Text>Restaurants List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  searchArea: {
+    padding: 16,
+    backgroundColor: "lightblue",
+  },
+  screenArea: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "lightyellow",
+  },
+});
