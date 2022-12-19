ad5fcd2fdb119f096064ddebcba96c17f669ce67
andmiguedev <andmiguedev@outlook.com>
2022-12-19 17:38:06 -0500

    Wrap restaurant app with a theme provider

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components/native";
+
 import RestaurantScreen from "./src/screens/Restaurants/restaurant.screen";
+import { theme } from "./src/theme/index";
 
 export default function App() {
   return (
     <>
-      <RestaurantScreen />
-      <ExpoStatusBar style="auto" />
+      <ThemeProvider theme={theme}>
+        <RestaurantScreen />
+        <ExpoStatusBar style="auto" />
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/theme/index.js
@@ -0,0 +1,5 @@
+const spaces = ["10px", "14px", "16px", "28px", "32px"];
+
+export const theme = {
+  spaces,
+};
